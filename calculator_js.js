db9ed128e8f631d7dd019f091da0a9a6561326cf
Wade Jones <wadedjones@gmail.com>
2021-09-27 22:14:07 -0700

    added more to js file

--- calculator_js.js
+++ calculator_js.js
@@ -1,4 +1,10 @@
 // CALCULATE
+let roundWinner = '';
+let playerScore = 0;
+let computerScore = 0;
+let compSelection = computerPlay();
+let playerSelection = 'paper'
+
 
 function computerPlay() {
     let randomPlay = Math.floor(Math.random() * 3);
@@ -12,6 +18,31 @@ function computerPlay() {
     };
 };
 
-const compPlay = computerPlay();
 
-console.log(compPlay);
+
+function playRound(playerSelection, compSelection) {
+    if (playerSelection === compSelection) {
+        roundWinner = 'Tie!'
+    }
+    if (
+        (playerSelection === 'rock' && compSelection === 'scissors') ||
+        (playerSelection === 'paper' && compSelection === 'rock') ||
+        (playerSelection === 'scissors' && compSelection === 'paper')
+    ) {
+        playerScore++;
+        roundWinner = 'You Won!'
+    }
+    if (
+        (compSelection === 'rock' && playerSelection === 'scissors') ||
+        (compSelection === 'paper' && playerSelection === 'rock') ||
+        (compSelection === 'scissors' && playerSelection === 'paper')
+    ) {
+        computerScore++
+        roundWinner = 'Computer Won!'
+    }
+}
+
+playRound('rock', compSelection)
+console.log(roundWinner);
+
+console.log(playerScore, computerScore)
