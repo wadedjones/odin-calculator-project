4d17a6fb00d8def2c34878ba84f1067f4aaa9d3a
Wade Jones <wadedjones@gmail.com>
2021-09-27 21:39:55 -0700

    added computerPlay function and json file

--- calculator_js.js
+++ calculator_js.js
@@ -1,1 +1,17 @@
 // CALCULATE
+
+function computerPlay() {
+    let randomPlay = Math.floor(Math.random() * 3);
+    switch (randomPlay) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    };
+};
+
+const compPlay = computerPlay();
+
+console.log(compPlay);
